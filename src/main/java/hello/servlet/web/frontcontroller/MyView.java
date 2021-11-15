1fe9d0fb01e73cb34e1f32269d2f789f032b4df6
Hyeri Jang <chjang5621@naver.com>
2021-11-15 18:33:02 +0900

    4. 프론트 컨트롤러 도입 - v3 : Model 추가

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -14,9 +15,29 @@ public class MyView {
         this.viewPath = viewPath;
     }
 
+    //NOTE: RequestDispatcher 란?
+    //- 현재 request 에 담긴 정보를 저장하고 있다가, 그 다음 페이지 , 다다음 페이지에서도 해당 정보를 볼 수 있게 계속 저장하는 기능
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
-    
+
+
+    // NOTE: V3부터 model 추가.
+
+    //  V2 까지는 model 을 사용하지 않았기 때문에
+    //  ControllerVX 인터페이스를 사용하는 컨트롤러 내에서 setAttribute 했었음
+
+    //  model 에 있는 정보를 다음 뷰에서 계속 사용하기 위해,
+    //  model 값을 꺼내서 request.setAttribute 에 다 넣음 (request 임. 주의!)
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
+    }
 }
---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -0,0 +1,67 @@
+package hello.servlet.web.frontcontroller.v3;
+
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+// NOTE : V2 보다 프론트 컨트롤러가 할 일 많아졌으나 다른 Controller 들은 간단해짐
+@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
+public class FrontControllerServletV3 extends HttpServlet {
+
+    private Map<String, ControllerV3> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV3() {
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV3.service");
+
+        String requestURI = request.getRequestURI();
+
+        ControllerV3 controller = controllerMap.get(requestURI);
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        Map<String, String> paramMap = createParamMap(request);
+
+        //paramMap을 사용하여 modelView 를 생성한다.
+        // Note : paramMap은 입력값, model은 서비스를 통해 얻은 결과값
+        ModelView modelView = controller.process(paramMap);
+
+        String viewName = modelView.getViewName();// NOTE: 논리 이름 반환
+        MyView view = viewResolver(viewName); // NOTE: viewResolver : 논리이름 -> 물리 경로
+
+        // render
+        view.render(modelView.getModel(), request, response);
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
